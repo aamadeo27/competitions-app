7cb743cf268cbaf7ce52c35fd4078c12a7e0a710
Albert Amadeo <Alberto.Amadeo@personal.com.py>
2023-03-22 21:56:08 -0300

    clean environment files

--- back/src/config/index.ts
+++ back/src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv'
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` })
+config({ path: '.env' })
 
 const { STEAM_API_KEY, API_JWT_SECRET } = process.env
 export const CREDENTIALS = process.env.CREDENTIALS === 'true'
