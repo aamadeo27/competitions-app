ad0d9dd33ca8f0409dc31787ec59cae22bb55e6a
albert <aamadeo@gmail.com>
2023-03-24 10:27:07 -0300

    add uncontrolled request admission btn (#16)

--- front/src/views/Competitions/index.tsx
+++ front/src/views/Competitions/index.tsx
@@ -4,14 +4,24 @@ import Header from '../../components/Header'
 import Loading from '../../components/Loading'
 import type { Competition } from '../../generated/graphql'
 import CompetitionItem from './CompetitionItem'
-import { competitionsQuery } from '../../graphql'
+import { competitionsQuery, userQuery } from '../../graphql'
+import { useUser } from '../../logic/client'
 
 export default function Competitions() {
   const { data, loading } = useQuery(competitionsQuery)
+  const user = useUser()?.user
+  const { data: userData } = useQuery(userQuery, {
+    variables: { id: user?.id },
+    skip: !!user,
+  })
 
   const competitions =
     data?.getCompetitions?.map((competition: Competition) => (
-      <CompetitionItem data={competition} key={competition.name} />
+      <CompetitionItem
+        data={competition}
+        key={competition.name}
+        user={userData}
+      />
     )) ?? []
 
   if (loading) {
